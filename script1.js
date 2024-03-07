7231a72f3f1f041d0979951b24b047650f499f0f
Eugene <horunzhy.wgen@gmail.com>
2024-03-07 22:06:48 +0200

    Finished with it

---
+++ script1.js
@@ -0,0 +1,36 @@
+const greetingEl = document.querySelector('.greeting');
+// const containerEl = document.querySelector('.container');
+// const nameEl = document.querySelector('.name');
+
+setTimeout(() => {
+  const script = document.createElement('script');
+  script.type = 'module';
+  script.src = './script.js';
+
+  document.body.appendChild(script);
+
+  document.body.removeChild(greetingEl);
+}, 3500);
+
+// if (window.innerWidth < 1110) {
+//   document.body.removeChild(greetingEl);
+//   document.body.removeChild(containerEl);
+//   document.body.removeChild(nameEl);
+
+//   const lowWidthSpan = document.createElement('span');
+
+//   lowWidthSpan.innerText = 'Use more wide screen. (min. width = 1110px)';
+//   lowWidthSpan.classList.add('error');
+
+//   document.body.appendChild(lowWidthSpan);
+// } else {
+//   setTimeout(() => {
+//     const script = document.createElement('script');
+//     script.type = 'module';
+//     script.src = './script.js';
+  
+//     document.body.appendChild(script);
+  
+//     document.body.removeChild(greetingEl);
+//   }, 38000);
+// }
